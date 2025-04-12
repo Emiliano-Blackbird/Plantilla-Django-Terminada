9f143757ebdbec147c2345a5f622a3b658564c69
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-04-12 14:49:00 +0200

    Se añadió formularios al proyecto

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 from courses.models import Course
 from blog.models import Post
+from .forms import ContactForms
 
 
 # Vistas generales de la app
@@ -28,4 +29,27 @@ def register(request):
 
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    if request.POST:
+        formulario = ContactForms(request.POST)
+
+        if formulario.is_valid():
+            nombre = formulario.cleaned_data['nombre']
+            email = formulario.cleaned_data['email']
+            comentario = formulario.cleaned_data['comentario']
+            print(f"Se ha enviado un correo a {nombre} procedente de email {email} con el texto {comentario}")
+            context = {
+                "form": formulario,
+                "success": True
+            }
+            return render(request, "core/contact.html", context)
+        else:
+            context = {
+                "form": formulario,
+            }
+            return render(request, "core/contact.html", context)
+
+    formulario = ContactForms()
+    context = {
+        "form": formulario,
+    }
+    return render(request, "core/contact.html", context)
